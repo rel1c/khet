64b51a2a26f52f44de667197b1b14ab5045f9963
Adam Peterson <mail@adamjp.com>
2020-10-16 10:02:51 -0500

    hit resolution and class reconfig

--- const.py
+++ const.py
@@ -1,17 +1,14 @@
 from enum import Enum
 
-
 class Color(Enum):
     SILVER = 0
     RED = 1
-
 
 class Direction(Enum):
     NORTH = 0
     EAST = 1
     SOUTH = 2
     WEST = 3
-
 
 class Face(Enum):
     REFLECT_POS = 0
@@ -20,10 +17,9 @@ class Face(Enum):
     DESTROY = 3
     GAME = 4
 
-
 class Kind(Enum):
-    PHARAOH = 0
-    SCARAB = 1
-    ANUBIS = 2
-    PYRAMID = 3
+    ANUBIS = 0
+    PHARAOH = 1
+    PYRAMID = 2
+    SCARAB = 3
     SPHINX = 4
--- piece.py
+++ piece.py
@@ -1,68 +1,93 @@
-from constants import Color, Direction, Face, Kind
-
+from const import Color, Direction, Face, Kind
 
 class Piece(object):
 
+    FACES_ANUBIS = ()
+    FACES_PHARAOH = (Face.GAME, Face.GAME, Face.GAME, Face.GAME) 
+    FACES_PYRAMID = (Face.REFLECT_NEG, Face.REFLECT_POS, Face.DESTROY, Face.DESTROY)
+    FACES_SCARAB = (Face.REFLECT_NEG, Face.REFLECT_POS, Face.REFLECT_NEG, Face.REFLECT_POS)
+    FACES_SPHINX= (Face.ABSORB, Face.ABSORB, Face.ABSORB, Face.ABSORB)
+
     def __init__(self, kind, color, direction=Direction.NORTH):
-        self.__kind = kind
-        self.__color = color
-        self.__direction = direction
-        self.__faces = [None] * 4                               # [NORTH, EAST,SOUTH, WEST]
+        self.kind = kind
+        self.color = color
+        self.direction = direction
+        self.faces = [None] * 4
+        self.move = True
 
     def getKind(self):
-        return self.__kind
+        return self.kind #TODO eliminate all get/set, use attributes
 
     def getColor(self):
-        return self.__color
+        return self.color
 
     def getDirection(self):
-        return self.__direction
+        return self.direction
 
     def getFace(self, laser_direction):
-        face_hit = (self.__direction.value + laser_direction.value) % 4    #TODO this is incorrect!
-        return self.__faces[face_hit]
+        face_hit = (2 - laser_direction.value + self.direction.value) % 4
+        print(face_hit)
+        return self.faces[face_hit]
 
     def rotatePositive(self):
-        self.__direction = Direction((self.__direction.value + 1) % 4)     # 4 Directions
+        self.direction = Direction((self.direction.value + 1) % 4)
 
     def rotateNegative(self):
-        self.__direction = Direction((self.__direction.value - 1) % 4)
+        self.direction = Direction((self.direction.value - 1) % 4)
 
     def __str__(self):
-        raise NotImplementedError('String method not implemented.')
-
+        return self.color.name + '_' + self.kind.name + '_' + self.direction.name
 
 class Scarab(Piece):
 
+#  \`\
+#   \ \
+#    \_\
+
     def __init__(self, color):
         Piece.__init__(self, Kind.SCARAB, color)
-
-    def __str__(self):
-        return 'Scarab'
-
+        self.faces = list(Piece.FACES_SCARAB)
 
 class Anubis(Piece):
 
+#  ,===,
+#  |   |
+#  |___|
+
     def __init__(self, color):
         Piece.__init__(self, Kind.ANUBIS, color)
+        self.faces[0] = Face.ABSORB
+        self.faces[1] = Face.DESTROY
+        self.faces[2] = Face.DESTROY
+        self.faces[3] = Face.DESTROY
 
-    def __str__(self):
-        return 'Anubis'
+class Pharaoh(Piece):
 
+#  |vvv|
+#  |   |
+#  |___|
+
+    def __init__(self, color):
+        Piece.__init__(self, Kind.PHARAOH, color)
+        self.faces = list(FACES_PHARAOH)
 
 class Pyramid(Piece):
 
+#   |\
+#   | \
+#   |__\
+
     def __init__(self, color):
         Piece.__init__(self, Kind.PYRAMID, color)
-
-    def __str__(self):
-        return 'Pyramid'
-
+        self.faces = list(FACES_PYRAMID)
 
 class Sphinx(Piece):
 
+#   )|(
+#  |   |
+#  |___|
+
     def __init__(self, color):
         Piece.__init__(self, Kind.SPHINX, color)
-
-    def __str__(self):
-        return 'Sphinx'
+        self.faces = list(FACES_SPHINX)
+        self.move = False
