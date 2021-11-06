c8857efdf8aeece270ac4aa116db06e968ee72d0
Adam Peterson <mail@adamjp.com>
2021-11-05 22:30:00 -0500

    stub board position functions

--- include/board.h
+++ include/board.h
@@ -1,24 +1,125 @@
 #ifndef BOARD_H_
 #define BOARD_H_
 
+#include <vector>
+
 namespace khet {
 
-struct Board {
-  static const Bitboard squares_red;
-  static const Bitboard squares_silver;
+class Board {
+public:
+  Bitboard getMoves(const Square) const;
 
-  Bitboard red;
-  Bitboard silver;
-  Bitboard north;
-  Bitboard east;
-  Bitboard south;
-  Bitboard west;
+  Bitboard getSquaresRed();
+  Bitboard getSquaresSilver();
+  Bitboard getRed();
+  Bitboard getSilver();
+                     
+  Bitboard getNorth();
+  Bitboard getEast();
+  Bitboard getSouth();
+  Bitboard getWest();
+                     
+  Bitboard getAnubis();
+  Bitboard getRedAnubis();
+  Bitboard getSilverAnubis();
+  Bitboard getAnubisNorth();
+  Bitboard getAnubisEast();
+  Bitboard getAnubisSouth();
+  Bitboard getAnubisWest();
+  Bitboard getRedAnubisNorth();
+  Bitboard getRedAnubisEast();
+  Bitboard getRedAnubisSouth();
+  Bitboard getRedAnubisWest();
+  Bitboard getSilverAnubisNorth();
+  Bitboard getSilverAnubisEast();
+  Bitboard getSilverAnubisSouth();
+  Bitboard getSilverAnubisWest();
+                     
+  Bitboard getPharaoh();
+  Bitboard getRedPharaoh();
+  Bitboard getSilverPharaoh();
+  Bitboard getPharaohNorth();
+  Bitboard getPharaohEast();
+  Bitboard getPharaohSouth();
+  Bitboard getPharaohWest();
+  Bitboard getRedPharaohNorth();
+  Bitboard getRedPharaohEast();
+  Bitboard getRedPharaohSouth();
+  Bitboard getRedPharaohWest();
+  Bitboard getSilverPharaohNorth();
+  Bitboard getSilverPharaohEast();
+  Bitboard getSilverPharaohSouth();
+  Bitboard getSilverPharaohWest();
+                     
+  Bitboard getPyramid();
+  Bitboard getRedPyramid();
+  Bitboard getSilverPyramid();
+  Bitboard getPyramidNorth();
+  Bitboard getPyramidEast();
+  Bitboard getPyramidSouth();
+  Bitboard getPyramidWest();
+  Bitboard getRedPyramidNorth();
+  Bitboard getRedPyramidEast();
+  Bitboard getRedPyramidSouth();
+  Bitboard getRedPyramidWest();
+  Bitboard getSilverPyramidNorth();
+  Bitboard getSilverPyramidEast();
+  Bitboard getSilverPyramidSouth();
+  Bitboard getSilverPyramidWest();
+                     
+  Bitboard getScarab();
+  Bitboard getRedScarab();
+  Bitboard getSilverScarab();
+  Bitboard getScarabNorth();
+  Bitboard getScarabEast();
+  Bitboard getScarabSouth();
+  Bitboard getScarabWest();
+  Bitboard getRedScarabNorth();
+  Bitboard getRedScarabEast();
+  Bitboard getRedScarabSouth();
+  Bitboard getRedScarabWest();
+  Bitboard getSilverScarabNorth();
+  Bitboard getSilverScarabEast();
+  Bitboard getSilverScarabSouth();
+  Bitboard getSilverScarabWest();
+                     
+  Bitboard getSphinx();
+  Bitboard getRedSphinx();
+  Bitboard getSilverSphinx();
+  Bitboard getSphinxNorth();
+  Bitboard getSphinxEast();
+  Bitboard getSphinxSouth();
+  Bitboard getSphinxWest();
+  Bitboard getRedSphinxNorth();
+  Bitboard getRedSphinxEast();
+  Bitboard getRedSphinxSouth();
+  Bitboard getRedSphinxWest();
+  Bitboard getSilverSphinxNorth();
+  Bitboard getSilverSphinxEast();
+  Bitboard getSilverSphinxSouth();
+  Bitboard getSilverSphinxWest();
+private:
+  static const std::vector<Bitboard> moves_;
 
-  Bitboard anubis;
-  Bitboard pharaoh;
-  Bitboard pyramid;
-  Bitboard scarab;
-  Bitboard sphinx;
+  static const Bitboard squares_red_;
+  static const Bitboard squares_silver_;
+
+  static const Bitboard empty_;
+  static const Bitboard full_;
+
+  Bitboard red_;
+  Bitboard silver_;
+
+  Bitboard north_;
+  Bitboard east_;
+  Bitboard south_;
+  Bitboard west_;
+
+  Bitboard anubis_;
+  Bitboard pharaoh_;
+  Bitboard pyramid_;
+  Bitboard scarab_;
+  Bitboard sphinx_;
 };
 
 } // namespace khet
--- src/board.cc
+++ src/board.cc
@@ -2,11 +2,343 @@
 
 namespace khet {
 
-Board::squares_red = initBitboard(
+Bitboard Bitboard::getSquaresRed() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSquaresSilver() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRed() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilver() {
+  return empty_;
+}
+
+Bitboard Bitboard::getNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getWest() {
+  return empty_;
+}
+
+Bitboard Bitboard::getAnubis() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedAnubis() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverAnubis() {
+  return empty_;
+}
+
+Bitboard Bitboard::getAnubisNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getAnubisEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getAnubisSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getAnubisWest() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedAnubisNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedAnubisEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedAnubisSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedAnubisWest() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverAnubisNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverAnubisEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverAnubisSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverAnubisWest() {
+  return empty_;
+}
+
+Bitboard Bitboard::getPharaoh() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedPharaoh() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverPharaoh() {
+  return empty_;
+}
+
+Bitboard Bitboard::getPharaohNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getPharaohEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getPharaohSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getPharaohWest() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedPharaohNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedPharaohEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedPharaohSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedPharaohWest() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverPharaohNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverPharaohEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverPharaohSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverPharaohWest() {
+  return empty_;
+}
+
+Bitboard Bitboard::getPyramid() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedPyramid() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverPyramid() {
+  return empty_;
+}
+
+Bitboard Bitboard::getPyramidNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getPyramidEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getPyramidSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getPyramidWest() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedPyramidNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedPyramidEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedPyramidSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedPyramidWest() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverPyramidNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverPyramidEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverPyramidSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverPyramidWest() {
+  return empty_;
+}
+
+Bitboard Bitboard::getScarab() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedScarab() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverScarab() {
+  return empty_;
+}
+
+Bitboard Bitboard::getScarabNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getScarabEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getScarabSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getScarabWest() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedScarabNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedScarabEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedScarabSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedScarabWest() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverScarabNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverScarabEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverScarabSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverScarabWest() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSphinx() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedSphinx() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverSphinx() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSphinxNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSphinxEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSphinxSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSphinxWest() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedSphinxNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedSphinxEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedSphinxSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getRedSphinxWest() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverSphinxNorth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverSphinxEast() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverSphinxSouth() {
+  return empty_;
+}
+
+Bitboard Bitboard::getSilverSphinxWest() {
+  return empty_;
+}
+
+Board::bb_squares_red = initBitboard(
   std::vector<int> {A1, A2, A3, A4, A5, A6, A7, A8, I1, I8}
 );
 
-Board::squares_silver = initBitboard(
+Board::bb_squares_silver = initBitboard(
   std::vector<int> {B1, B8, J1, J2, J3, J4, J5, J6, J7, J8}
 );
 
