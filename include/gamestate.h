ff1bca31b314d21a32650865e63e64324bcf769e
Adam Peterson <mail@adamjp.com>
2021-11-07 20:27:39 -0600

    hardcode layout configs

--- include/gamestate.h
+++ include/gamestate.h
@@ -3,10 +3,16 @@
 
 namespace khet {
 
+enum Layout {
+  CLASSIC,
+  IMHOTEP,
+  DYNASTY
+};
+
 class Gamestate {
 public:
-  Gamestate();
-  void initLayout();
+  Gamestate(Layout);
+  void initLayout(Layout);
 private:
   Board board_;
 };
