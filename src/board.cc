fc44636e4308a02de45b6ccef42c64c2d0c8a687
Adam Peterson <mail@adamjp.com>
2021-11-10 23:46:28 -0600

    stub generating move bitboards

--- src/board.cc
+++ src/board.cc
@@ -10,4 +10,27 @@ Board::bb_squares_silver = initBitboard(
   std::vector<int> {B1, B8, J1, J2, J3, J4, J5, J6, J7, J8}
 );
 
+const std::array<Bitboard, NFILES * NRANKS> Board::moves_ {
+  bool right, left, top, bottom;
+  std::vector<Square> squares;
+  for (int i = 0; i < moves_.length(); i++) {
+    if (i - 1 % NFILES == 0) {
+      right = true;
+    }
+    else if (i % NFILES == 0) {
+      left = true;
+    }
+    if (i + NFILES >= NFILES * NRANKS) {
+      top = true;
+    }
+    else if (i - NFILES < 0) {
+      bottom = true;
+    }
+
+    if (!top && !left) {
+
+    }
+  }
+}
+
 } // namespace khet
