a828849f4ddc8922687d55085a14a2b954d714b1
Adam Peterson <mail@adamjp.com>
2021-12-14 20:11:12 -0600

    add board tests

--- tests/tests.cc
+++ tests/tests.cc
@@ -148,60 +148,403 @@ TEST_F(BoardTest, PknBoardTest) {
   EXPECT_TRUE(b.isPieceAt(E8));
 }
 
-TEST_F(BoardTest, getColorTest) {
-  GTEST_SKIP();//TODO
+TEST_F(BoardTest, getColorClassicTest) {
+  Bitboard red_c = initBitboard(std::vector<Square> {
+    A4, A5, A8, C7, E5, E8, F5, F8, G3, G8, H4, H5, H8
+  });
+  Bitboard silver_c = initBitboard(std::vector<Square> {
+    C1, C4, C5, D1, D6, E1, E4, F1, F4, H2, J1, J4, J5
+  });
+  EXPECT_EQ(red_c, board_c.getColor(RED));
+  EXPECT_EQ(silver_c, board_c.getColor(SILVER));
 }
 
-TEST_F(BoardTest, getDirectionTest) {
-  GTEST_SKIP();//TODO
+TEST_F(BoardTest, getColorDynastyTest) {
+  Bitboard red_d = initBitboard(std::vector<Square> {
+    D4, F4, A5, C5, A6, E6, F6, G6, F7, A8, E8, F8, G8
+  });
+  Bitboard silver_d = initBitboard(std::vector<Square> {
+    D1, E1, F1, J1, E2, D3, E3, F3, J3, H4, J4, E5, G5
+  });
+  EXPECT_EQ(red_d, board_d.getColor(RED));
+  EXPECT_EQ(silver_d, board_d.getColor(SILVER));
 }
 
-TEST_F(BoardTest, getPieceTest) {
-  GTEST_SKIP();//TODO
+TEST_F(BoardTest, getColorImhotepTest) {
+  Bitboard red_i = initBitboard(std::vector<Square> {
+    G3, A4, F4, I4, A5, F5, I5, G6, A8, E8, F8, G8, H8
+  });
+  Bitboard silver_i = initBitboard(std::vector<Square> {
+    C1, D1, E1, F1, J1, D3, B4, E4, J4, B5, E5, J5, D6
+  });
+  EXPECT_EQ(red_i, board_i.getColor(RED));
+  EXPECT_EQ(silver_i, board_i.getColor(SILVER));
 }
 
-TEST_F(BoardTest, isPieceAtTest) {
-  GTEST_SKIP();//TODO
+TEST_F(BoardTest, getDirectionClassicTest) {
+  Bitboard north_c = initBitboard(std::vector<Square> {
+    A5, D1, E1, E5, F1, H2, H4, J1
+  });
+  Bitboard east_c = initBitboard(std::vector<Square> {
+    A4, F5, G3, H5, H8
+  });
+  Bitboard south_c = initBitboard(std::vector<Square> {
+    A8, C5, C7, E8, F4, F8, G8, J4
+  });
+  Bitboard west_c = initBitboard(std::vector<Square> {
+    C1, C4, D6, E4, J5
+  });
+  EXPECT_EQ(north_c, board_c.getDirection(NORTH));
+  EXPECT_EQ(east_c, board_c.getDirection(EAST));
+  EXPECT_EQ(south_c, board_c.getDirection(SOUTH));
+  EXPECT_EQ(west_c, board_c.getDirection(WEST));
 }
 
-TEST_F(BoardTest, getColorAtTest) {
-  GTEST_SKIP();//TODO
+TEST_F(BoardTest, getDirectionDynastyTest) {
+  Bitboard north_d = initBitboard(std::vector<Square> {
+    A6, E1, E2, E3, F1, F3, H4, J1
+  });
+  Bitboard east_d = initBitboard(std::vector<Square> {
+    A5, D3, F4, G5, G8
+  });
+  Bitboard south_d = initBitboard(std::vector<Square> {
+    A8, C5, E6, E8, F6, F7, F8, J3
+  });
+  Bitboard west_d = initBitboard(std::vector<Square> {
+    D1, D4, E5, G6, J4
+  });
+  EXPECT_EQ(north_d, board_d.getDirection(NORTH));
+  EXPECT_EQ(east_d, board_d.getDirection(EAST));
+  EXPECT_EQ(south_d, board_d.getDirection(SOUTH));
+  EXPECT_EQ(west_d, board_d.getDirection(WEST));
 }
 
-TEST_F(BoardTest, getPieceAtTest) {
-  GTEST_SKIP();//TODO
+TEST_F(BoardTest, getDirectionImhotepTest) {
+  Bitboard north_i = initBitboard(std::vector<Square> {
+    A5, D1, E1, F1, G6, I4, J1
+  });
+  Bitboard east_i = initBitboard(std::vector<Square> {
+    A4, E5, F5, G3, H8, I5
+  });
+  Bitboard south_i = initBitboard(std::vector<Square> {
+    A8, B5, D3, E8, F8, G8, J4
+  });
+  Bitboard west_i = initBitboard(std::vector<Square> {
+    B4, C1, D6, E4, F4, J5
+  });
+  EXPECT_EQ(north_i, board_i.getDirection(NORTH));
+  EXPECT_EQ(east_i, board_i.getDirection(EAST));
+  EXPECT_EQ(south_i, board_i.getDirection(SOUTH));
+  EXPECT_EQ(west_i, board_i.getDirection(WEST));
 }
 
-TEST_F(BoardTest, getAllPiecesTest) {
-  GTEST_SKIP();//TODO
+TEST_F(BoardTest, getPieceClassicTest) {
+  Bitboard anubis_c = initBitboard(std::vector<Square> {
+    D1, E8, F1, G8
+  });
+  Bitboard pharaoh_c = initBitboard(std::vector<Square> {
+    E1, F8
+  });
+  Bitboard pyramid_c = initBitboard(std::vector<Square> {
+    A4, A5, C1, C4, C5, C7, D6, G3, H2, H4, H5, H8, J4, J5
+  });
+  Bitboard scarab_c = initBitboard(std::vector<Square> {
+    E4, E5, F4, F5
+  });
+  Bitboard sphinx_c = initBitboard(std::vector<Square> {
+    A8, J1
+  });
+  EXPECT_EQ(anubis_c, board_c.getPiece(ANUBIS));
+  EXPECT_EQ(pharaoh_c, board_c.getPiece(PHARAOH));
+  EXPECT_EQ(pyramid_c, board_c.getPiece(PYRAMID));
+  EXPECT_EQ(scarab_c, board_c.getPiece(SCARAB));
+  EXPECT_EQ(sphinx_c, board_c.getPiece(SPHINX));
 }
 
-TEST_F(BoardTest, getSwappableTest) {
-  GTEST_SKIP();//TODO
+TEST_F(BoardTest, getPieceDynastyTest) {
+  Bitboard anubis_d = initBitboard(std::vector<Square> {
+    E1, E3, F6, F8
+  });
+  Bitboard pharaoh_d = initBitboard(std::vector<Square> {
+    E2, F7
+  });
+  Bitboard pyramid_d = initBitboard(std::vector<Square> {
+    A5, A6, D1, D4, E5, E6, E8, F1, F3, F4, G5, G8, J3, J4
+  });
+  Bitboard scarab_d = initBitboard(std::vector<Square> {
+    C5, D3, G6, H4
+  });
+  Bitboard sphinx_d = initBitboard(std::vector<Square> {
+    A8, J1
+  });
+  EXPECT_EQ(anubis_d, board_d.getPiece(ANUBIS));
+  EXPECT_EQ(pharaoh_d, board_d.getPiece(PHARAOH));
+  EXPECT_EQ(pyramid_d, board_d.getPiece(PYRAMID));
+  EXPECT_EQ(scarab_d, board_d.getPiece(SCARAB));
+  EXPECT_EQ(sphinx_d, board_d.getPiece(SPHINX));
+}
+
+TEST_F(BoardTest, getPieceImhotepTest) {
+  Bitboard anubis_i = initBitboard(std::vector<Square> {
+    D1, E8, F1, G8
+  });
+  Bitboard pharaoh_i = initBitboard(std::vector<Square> {
+    E1, F8
+  });
+  Bitboard pyramid_i = initBitboard(std::vector<Square> {
+    A4, A5, B4, B5, D3, D6, E5, F4, G3, G6, I4, I5, J4, J5
+  });
+  Bitboard scarab_i = initBitboard(std::vector<Square> {
+    C1, E4, F5, H8
+  });
+  Bitboard sphinx_i = initBitboard(std::vector<Square> {
+    A8, J1
+  });
+  EXPECT_EQ(anubis_i, board_i.getPiece(ANUBIS));
+  EXPECT_EQ(pharaoh_i, board_i.getPiece(PHARAOH));
+  EXPECT_EQ(pyramid_i, board_i.getPiece(PYRAMID));
+  EXPECT_EQ(scarab_i, board_i.getPiece(SCARAB));
+  EXPECT_EQ(sphinx_i, board_i.getPiece(SPHINX));
+}
+
+TEST_F(BoardTest, getColorAtClassicTest) {
+  SquareVec red_c = {
+    A4, A5, A8, C7, E5, E8, F5, F8, G3, G8, H4, H5, H8
+  };
+  SquareVec silver_c = {
+    C1, C4, C5, D1, D6, E1, E4, F1, F4, H2, J1, J4, J5
+  };
+  Bitboard red_bb_c = board_c.getColor(RED);
+  for (auto s : red_c) {
+    EXPECT_TRUE(red_bb_c[s]);
+  }
+  Bitboard silver_bb_c = board_c.getColor(SILVER);
+  for (auto s : silver_c) {
+    EXPECT_TRUE(silver_bb_c[s]);
+  }
+}
+
+TEST_F(BoardTest, getColorAtDynastyTest) {
+  SquareVec red_d = {
+    D4, F4, A5, C5, A6, E6, F6, G6, F7, A8, E8, F8, G8
+  };
+  SquareVec silver_d = {
+    D1, E1, F1, J1, E2, D3, E3, F3, J3, H4, J4, E5, G5
+  };
+  Bitboard red_bb_d = board_d.getColor(RED);
+  for (auto s : red_d) {
+    EXPECT_TRUE(red_bb_d[s]);
+  }
+  Bitboard silver_bb_d = board_d.getColor(SILVER);
+  for (auto s : silver_d) {
+    EXPECT_TRUE(silver_bb_d[s]);
+  }
+}
+
+TEST_F(BoardTest, getColorAtImhotepTest) {
+  SquareVec red_i = {
+    G3, A4, F4, I4, A5, F5, I5, G6, A8, E8, F8, G8, H8
+  };
+  SquareVec silver_i = {
+    C1, D1, E1, F1, J1, D3, B4, E4, J4, B5, E5, J5, D6
+  };
+  Bitboard red_bb_i = board_i.getColor(RED);
+  for (auto s : red_i) {
+    EXPECT_TRUE(red_bb_i[s]);
+  }
+  Bitboard silver_bb_i = board_i.getColor(SILVER);
+  for (auto s : silver_i) {
+    EXPECT_TRUE(silver_bb_i[s]);
+  }
+}
+
+TEST_F(BoardTest, getPieceAtClassicTest) {
+  SquareVec anubis_c = {
+    D1, E8, F1, G8
+  };
+  SquareVec pharaoh_c = {
+    E1, F8
+  };
+  SquareVec pyramid_c = {
+    A4, A5, C1, C4, C5, C7, D6, G3, H2, H4, H5, H8, J4, J5
+  };
+  SquareVec scarab_c = {
+    E4, E5, F4, F5
+  };
+  SquareVec sphinx_c = {
+    A8, J1
+  };
+  Bitboard anubis_bb_c = board_c.getPiece(ANUBIS);
+  for (auto s : anubis_c) {
+    EXPECT_TRUE(anubis_bb_c[s]);
+  }
+  Bitboard pharaoh_bb_c = board_c.getPiece(PHARAOH);
+  for (auto s : pharaoh_c) {
+    EXPECT_TRUE(pharaoh_bb_c[s]);
+  }
+  Bitboard pyramid_bb_c = board_c.getPiece(PYRAMID);
+  for (auto s : pyramid_c) {
+    EXPECT_TRUE(pyramid_bb_c[s]);
+  }
+  Bitboard scarab_bb_c = board_c.getPiece(SCARAB);
+  for (auto s : scarab_c) {
+    EXPECT_TRUE(scarab_bb_c[s]);
+  }
+  Bitboard sphinx_bb_c = board_c.getPiece(SPHINX);
+  for (auto s : sphinx_c) {
+    EXPECT_TRUE(sphinx_bb_c[s]);
+  }
+}
+
+TEST_F(BoardTest, getPieceAtDynastyTest) {
+  SquareVec anubis_d = {
+    E1, E3, F6, F8
+  };
+  SquareVec pharaoh_d = {
+    E2, F7
+  };
+  SquareVec pyramid_d = {
+    A5, A6, D1, D4, E5, E6, E8, F1, F3, F4, G5, G8, J3, J4
+  };
+  SquareVec scarab_d = {
+    C5, D3, G6, H4
+  };
+  SquareVec sphinx_d = {
+    A8, J1
+  };
+  Bitboard anubis_bb_d = board_d.getPiece(ANUBIS);
+  for (auto s : anubis_d) {
+    EXPECT_TRUE(anubis_bb_d[s]);
+  }
+  Bitboard pharaoh_bb_d = board_d.getPiece(PHARAOH);
+  for (auto s : pharaoh_d) {
+    EXPECT_TRUE(pharaoh_bb_d[s]);
+  }
+  Bitboard pyramid_bb_d = board_d.getPiece(PYRAMID);
+  for (auto s : pyramid_d) {
+    EXPECT_TRUE(pyramid_bb_d[s]);
+  }
+  Bitboard scarab_bb_d = board_d.getPiece(SCARAB);
+  for (auto s : scarab_d) {
+    EXPECT_TRUE(scarab_bb_d[s]);
+  }
+  Bitboard sphinx_bb_d = board_d.getPiece(SPHINX);
+  for (auto s : sphinx_d) {
+    EXPECT_TRUE(sphinx_bb_d[s]);
+  }
+}
+
+TEST_F(BoardTest, getPieceAtImhotepTest) {
+  SquareVec anubis_i = {
+    D1, E8, F1, G8
+  };
+  SquareVec pharaoh_i = {
+    E1, F8
+  };
+  SquareVec pyramid_i = {
+    A4, A5, B4, B5, D3, D6, E5, F4, G3, G6, I4, I5, J4, J5
+  };
+  SquareVec scarab_i = {
+    C1, E4, F5, H8
+  };
+  SquareVec sphinx_i = {
+    A8, J1
+  };
+  Bitboard anubis_bb_i = board_i.getPiece(ANUBIS);
+  for (auto s : anubis_i) {
+    EXPECT_TRUE(anubis_bb_i[s]);
+  }
+  Bitboard pharaoh_bb_i = board_i.getPiece(PHARAOH);
+  for (auto s : pharaoh_i) {
+    EXPECT_TRUE(pharaoh_bb_i[s]);
+  }
+  Bitboard pyramid_bb_i = board_i.getPiece(PYRAMID);
+  for (auto s : pyramid_i) {
+    EXPECT_TRUE(pyramid_bb_i[s]);
+  }
+  Bitboard scarab_bb_i = board_i.getPiece(SCARAB);
+  for (auto s : scarab_i) {
+    EXPECT_TRUE(scarab_bb_i[s]);
+  }
+  Bitboard sphinx_bb_i = board_i.getPiece(SPHINX);
+  for (auto s : sphinx_i) {
+    EXPECT_TRUE(sphinx_bb_i[s]);
+  }
+}
+
+TEST_F(BoardTest, getAllPiecesClassicTest) {
+  Bitboard expect_all_c = initBitboard(
+    SquareVec {
+      A4, A5, A8, C1, C4, C5, C7, D1, D6, E1, E4, E5, E8,
+      F1, F4, F5, F8, G3, G8, H2, H4, H5, H8, J1, J4, J5
+    }
+  );
+  Bitboard actual_all_c = board_c.getAllPieces();
+  EXPECT_EQ(expect_all_c, actual_all_c);
+}
+
+TEST_F(BoardTest, getAllPiecesDynastyTest) {
+  Bitboard expect_all_d = initBitboard(
+    SquareVec {
+      A5, A6, A8, C5, D1, D3, D4, E1, E2, E3, E5, E6, E8,
+      F1, F3, F4, F6, F7, F8, G5, G6, G8, H4, J1, J3, J4
+    }
+  );
+  Bitboard actual_all_d = board_d.getAllPieces();
+  EXPECT_EQ(expect_all_d, actual_all_d);
+}
+
+TEST_F(BoardTest, getAllPiecesImhotepTest) {
+  Bitboard expect_all_i = initBitboard(
+    SquareVec {
+      A4, A5, A8, B4, B5, C1, D1, D3, D6, E1, E4, E5, E8,
+      F1, F4, F5, F8, G3, G6, G8, H8, I4, I5, J1, J4, J5
+    }
+  );
+  Bitboard actual_all_i = board_i.getAllPieces();
+  EXPECT_EQ(expect_all_i, actual_all_i);
+}
+
+TEST_F(BoardTest, getSwappableClassicTest) {
+  Bitboard expect_swap_c = initBitboard(
+    SquareVec {
+      A4, A5, C1, C4, C5, C7, D1, D6, E8,
+      F1, G3, G8, H2, H4, H5, H8, J4, J5
+    }
+  );
+  Bitboard actual_swap_c = board_c.getSwappable();
+  EXPECT_EQ(expect_swap_c, actual_swap_c);
+}
+
+TEST_F(BoardTest, getSwappableDynastyTest) {
+  Bitboard expect_swap_d = initBitboard(
+    SquareVec {
+      A5, A6, D1, D4, E1, E3, E5, E6, E8,
+      F1, F3, F4, F6, F8, G5, G8, J3, J4
+    }
+  );
+  Bitboard actual_swap_d = board_d.getSwappable();
+  EXPECT_EQ(expect_swap_d, actual_swap_d);
+}
+
+TEST_F(BoardTest, getSwappableImhotepTest) {
+  Bitboard expect_swap_i = initBitboard(
+    SquareVec {
+      A4, A5, B4, B5, D1, D3, D6, E5, E8,
+      F1, F4, G3, G6, G8, I4, I5, J4, J5
+    }
+  );
+  Bitboard actual_swap_i = board_i.getSwappable();
+  EXPECT_EQ(expect_swap_i, actual_swap_i);
 }
 
 TEST_F(BoardTest, getPlayerTest) {
-  GTEST_SKIP();//TODO
-}
-
-// Display the board in a convenient format
-TEST_F(BoardTest, DisplayTest) {
-  Board b(CLASSIC);
-  ASSERT_EQ(b.getAllPieces().count(), 26);
-  CoutRedirect cr;
-  b.display();
-  std::string output = cr.getString();
-  std::string expect = 
-    "8 x...arap.. s...ssse.. SILVER\n"
-    "7 ..p....... ..s....... turn: 0\n"
-    "6 ...P...... ...w......\n"
-    "5 p.P.ss.p.P n.s.ne.e.w\n"
-    "4 p.P.SS.p.P e.w.ws.n.s\n"
-    "3 ......p... ......e...\n"
-    "2 .......P.. .......n..\n"
-    "1 ..PARA...X ..wnnn...n\n"
-    "  abcdefghij abcdefghij\n";
-  EXPECT_EQ(output, expect);
+  std::string pkn_s = "5RS2X/5A4/5S3p/0/0/p3s5/4a5/x2sr5 nsnnssnnssns s 0";
+  Board bs(pkn_s);
+  std::string pkn_r = "5RS2X/5A4/5S3p/0/0/p3s5/4a5/x2sr5 nsnnssnnssns r 0";
+  Board br(pkn_r);
+  EXPECT_EQ(bs.getPlayer(), SILVER);
+  EXPECT_EQ(br.getPlayer(), RED);
 }
 
 using pkn_tup_t = std::tuple<std::string, SquareVec, std::string>;
@@ -213,7 +556,7 @@ public:
   unsigned int n_pieces;
 };
 
-TEST_P(PknBoardTestsParam, PknBoardTest) {
+TEST_P(PknBoardTestsParam, PknBoardDisplayTest) {
   pkn = std::get<0>(GetParam());
   sv = std::get<1>(GetParam());
   expect = std::get<2>(GetParam());
@@ -298,206 +641,6 @@ INSTANTIATE_TEST_SUITE_P(
   )
 );
 
-/*
-// Test the mapping of bitboard notation to actual indices
-TEST_F(BoardTest, ConstructorClassicRedTest) {
-  Board classic = Board(CLASSIC);
-  std::string output, expect;
-  testing::internal::CaptureStdout();
-  displayBitboard(classic.red_);
-  output = testing::internal::GetCapturedStdout();
-  expect = 
-    "1000111100\n"
-    "0010000000\n"
-    "0000000000\n"
-    "1000110100\n"
-    "1000000100\n"
-    "0000001000\n"
-    "0000000000\n"
-    "0000000000\n";
-  EXPECT_EQ(output, expect);
-}
-
-TEST_F(BoardTest, ConstructorClassicSilverTest) {
-  Board classic = Board(CLASSIC);
-  std::string output, expect;
-  testing::internal::CaptureStdout();
-  displayBitboard(classic.silver_);
-  output = testing::internal::GetCapturedStdout();
-  expect = 
-    "0000000000\n"
-    "0000000000\n"
-    "0001000000\n"
-    "0010000001\n"
-    "0010110001\n"
-    "0000000000\n"
-    "0000000100\n"
-    "0011110001\n";
-  EXPECT_EQ(output, expect);
-}
-
-TEST_F(BoardTest, ConstructorClassicNorthTest) {
-  Board classic = Board(CLASSIC);
-  std::string output, expect;
-  testing::internal::CaptureStdout();
-  displayBitboard(classic.north_);
-  output = testing::internal::GetCapturedStdout();
-  expect = 
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "1000100000\n"
-    "0000000100\n"
-    "0000000000\n"
-    "0000000100\n"
-    "0001110001\n";
-  EXPECT_EQ(output, expect);
-}
-
-TEST_F(BoardTest, ConstructorClassicEastTest) {
-  Board classic = Board(CLASSIC);
-  std::string output, expect;
-  testing::internal::CaptureStdout();
-  displayBitboard(classic.east_);
-  output = testing::internal::GetCapturedStdout();
-  expect = 
-    "0000000100\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000010100\n"
-    "1000000000\n"
-    "0000001000\n"
-    "0000000000\n"
-    "0000000000\n";
-  EXPECT_EQ(output, expect);
-}
-
-TEST_F(BoardTest, ConstructorClassicSouthTest) {
-  Board classic = Board(CLASSIC);
-  std::string output, expect;
-  testing::internal::CaptureStdout();
-  displayBitboard(classic.south_);
-  output = testing::internal::GetCapturedStdout();
-  expect = 
-    "1000111000\n"
-    "0010000000\n"
-    "0000000000\n"
-    "0010000000\n"
-    "0000010001\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n";
-  EXPECT_EQ(output, expect);
-}
-
-TEST_F(BoardTest, ConstructorClassicWestTest) {
-  Board classic = Board(CLASSIC);
-  std::string output, expect;
-  testing::internal::CaptureStdout();
-  displayBitboard(classic.west_);
-  output = testing::internal::GetCapturedStdout();
-  expect = 
-    "0000000000\n"
-    "0000000000\n"
-    "0001000000\n"
-    "0000000001\n"
-    "0010100000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0010000000\n";
-  EXPECT_EQ(output, expect);
-}
-
-TEST_F(BoardTest, ConstructorClassicAnubisTest) {
-  Board classic = Board(CLASSIC);
-  std::string output, expect;
-  testing::internal::CaptureStdout();
-  displayBitboard(classic.anubis_);
-  output = testing::internal::GetCapturedStdout();
-  expect = 
-    "0000101000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0001010000\n";
-  EXPECT_EQ(output, expect);
-}
-
-TEST_F(BoardTest, ConstructorClassicPharaohTest) {
-  Board classic = Board(CLASSIC);
-  std::string output, expect;
-  testing::internal::CaptureStdout();
-  displayBitboard(classic.pharaoh_);
-  output = testing::internal::GetCapturedStdout();
-  expect = 
-    "0000010000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000100000\n";
-  EXPECT_EQ(output, expect);
-}
-
-TEST_F(BoardTest, ConstructorClassicPyramidTest) {
-  Board classic = Board(CLASSIC);
-  std::string output, expect;
-  testing::internal::CaptureStdout();
-  displayBitboard(classic.pyramid_);
-  output = testing::internal::GetCapturedStdout();
-  expect = 
-    "0000000100\n"
-    "0010000000\n"
-    "0001000000\n"
-    "1010000101\n"
-    "1010000101\n"
-    "0000001000\n"
-    "0000000100\n"
-    "0010000000\n";
-  EXPECT_EQ(output, expect);
-}
-
-TEST_F(BoardTest, ConstructorClassicScarabTest) {
-  Board classic = Board(CLASSIC);
-  std::string output, expect;
-  testing::internal::CaptureStdout();
-  displayBitboard(classic.scarab_);
-  output = testing::internal::GetCapturedStdout();
-  expect = 
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000110000\n"
-    "0000110000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n";
-  EXPECT_EQ(output, expect);
-}
-
-TEST_F(BoardTest, ConstructorClassicSphinxTest) {
-  Board classic = Board(CLASSIC);
-  std::string output, expect;
-  testing::internal::CaptureStdout();
-  displayBitboard(classic.sphinx_);
-  output = testing::internal::GetCapturedStdout();
-  expect = 
-    "1000000000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000000000\n"
-    "0000000001\n";
-  EXPECT_EQ(output, expect);
-}
-
 /// Action Tests ///////////////////////////////////////////////////////////////
 /*
 class ActionTest : public::testing::Test {};
@@ -510,7 +653,6 @@ TEST_F(ActionTest, SizeTest) {
 
 // Test ...
 TEST_F(ActionTest, OutStreamTest) {
-  GTEST_SKIP();//TODO
   //Action act;
   //testing::internal::CaptureStdout();
   //std::cout << act;
