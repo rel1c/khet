9a270f55e891935644fbfaeae0286af6568f86b6
Adam Peterson <mail@adamjp.com>
2022-01-11 18:43:26 -0600

    simplify rank and file definitions

--- src/bitboard.cc
+++ src/bitboard.cc
@@ -29,58 +29,26 @@ const std::array<std::string, NSQUARES> squareStrings {
 const Bitboard FILE_A = initBitboard(SquareVec {
   A1, A2, A3, A4, A5, A6, A7, A8
 });
-const Bitboard FILE_B = initBitboard(SquareVec {
-  B1, B2, B3, B4, B5, B6, B7, B8
-});
-const Bitboard FILE_C = initBitboard(SquareVec {
-  C1, C2, C3, C4, C5, C6, C7, C8
-});
-const Bitboard FILE_D = initBitboard(SquareVec {
-  D1, D2, D3, D4, D5, D6, D7, D8
-});
-const Bitboard FILE_E = initBitboard(SquareVec {
-  E1, E2, E3, E4, E5, E6, E7, E8
-});
-const Bitboard FILE_F = initBitboard(SquareVec {
-  F1, F2, F3, F4, F5, F6, F7, F8
-});
-const Bitboard FILE_G = initBitboard(SquareVec {
-  G1, G2, G3, G4, G5, G6, G7, G8
-});
-const Bitboard FILE_H = initBitboard(SquareVec {
-  H1, H2, H3, H4, H5, H6, H7, H8
-});
-const Bitboard FILE_I = initBitboard(SquareVec {
-  I1, I2, I3, I4, I5, I6, I7, I8
-});
-const Bitboard FILE_J = initBitboard(SquareVec {
-  J1, J2, J3, J4, J5, J6, J7, J8
-});
+const Bitboard FILE_B = FILE_A << 1;
+const Bitboard FILE_C = FILE_A << 2;
+const Bitboard FILE_D = FILE_A << 3;
+const Bitboard FILE_E = FILE_A << 4;
+const Bitboard FILE_F = FILE_A << 5;
+const Bitboard FILE_G = FILE_A << 6;
+const Bitboard FILE_H = FILE_A << 7;
+const Bitboard FILE_I = FILE_A << 8;
+const Bitboard FILE_J = FILE_A << 9;
 
 const Bitboard RANK_1 = initBitboard(SquareVec {
   A1, B1, C1, D1, E1, F1, G1, H1, I1, J1
 });
-const Bitboard RANK_2 = initBitboard(SquareVec {
-  A2, B2, C2, D2, E2, F2, G2, H2, I2, J2
-});
-const Bitboard RANK_3 = initBitboard(SquareVec {
-  A3, B3, C3, D3, E3, F3, G3, H3, I3, J3
-});
-const Bitboard RANK_4 = initBitboard(SquareVec {
-  A4, B4, C4, D4, E4, F4, G4, H4, I4, J4
-});
-const Bitboard RANK_5 = initBitboard(SquareVec {
-  A5, B5, C5, D5, E5, F5, G5, H5, I5, J5
-});
-const Bitboard RANK_6 = initBitboard(SquareVec {
-  A6, B6, C6, D6, E6, F6, G6, H6, I6, J6
-});
-const Bitboard RANK_7 = initBitboard(SquareVec {
-  A7, B7, C7, D7, E7, F7, G7, H7, I7, J7
-});
-const Bitboard RANK_8 = initBitboard(SquareVec {
-  A8, B8, C8, D8, E8, F8, G8, H8, I8, J8
-});
+const Bitboard RANK_2 = RANK_1 << (NFILES * 1);
+const Bitboard RANK_3 = RANK_1 << (NFILES * 2);
+const Bitboard RANK_4 = RANK_1 << (NFILES * 3);
+const Bitboard RANK_5 = RANK_1 << (NFILES * 4);
+const Bitboard RANK_6 = RANK_1 << (NFILES * 5);
+const Bitboard RANK_7 = RANK_1 << (NFILES * 6);
+const Bitboard RANK_8 = RANK_1 << (NFILES * 7);
 
 const std::array<Bitboard, NSQUARES> SQUARES {
   FILE_A & RANK_1,
